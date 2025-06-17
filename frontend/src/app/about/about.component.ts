1b79f280e700ce6ebaab342c09a0957e4322d048
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2025-06-17 14:35:48 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -79,10 +79,10 @@ export class AboutComponent implements OnInit {
   populateSlideshowFromFeedbacks () {
     this.feedbackService.find().subscribe((feedbacks) => {
       for (let i = 0; i < feedbacks.length; i++) {
-        const sanitizedComment = this.sanitizer.sanitize(1, feedbacks[i].comment) || '';
-        feedbacks[i].comment = `<span style="width: 90%; display:block;">${sanitizedComment}<br/> (${this.stars[feedbacks[i].rating]})</span>`;
-        feedbacks[i].comment = this.sanitizer.bypassSecurityTrustHtml(feedbacks[i].comment);
-        this.slideshowDataSource.push({ url: this.images[i % this.images.length], caption: feedbacks[i].comment });
+        const sanitizedComment = this.sanitizer.sanitize(1, feedbacks[i].comment) || ''
+        feedbacks[i].comment = `<span style="width: 90%; display:block;">${sanitizedComment}<br/> (${this.stars[feedbacks[i].rating]})</span>`
+        feedbacks[i].comment = this.sanitizer.bypassSecurityTrustHtml(feedbacks[i].comment)
+        this.slideshowDataSource.push({ url: this.images[i % this.images.length], caption: feedbacks[i].comment })
       }
     }, (err) => {
       console.log(err)
