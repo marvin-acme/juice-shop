5c582c798ea73a23ae5389fecf2579f33d224647
xygenibot <xygeni_scan@xygeni.io>
2026-05-13 03:56:53 +0000

    Fix javascript.insecure_randomness issue in data/datacreator.ts

--- data/datacreator.ts
+++ data/datacreator.ts
@@ -228,7 +228,7 @@ async function createRandomFakeUsers () {
     let text = ''
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-    for (let i = 0; i < length; i++) { text += possible.charAt(Math.floor(Math.random() * possible.length)) }
+    for (let i = 0; i < length; i++) { text += possible.charAt(Math.floor(crypto.randomBytes(1)[0] / 256 * possible.length)) }
 
     return text
   }
